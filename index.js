2c7ed3b7d7795297653a6f70a6660a9c15966c36
Joshua Noakes <53623449+Joshua-Noakes1@users.noreply.github.com>
2019-12-31 03:27:32 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const {
   Client,
   RichEmbed
 } = require('discord.js');
-const versnu = ("Version 3")
+const versnu = ("Version 2.01")
 const client = new Client();
 const prefex = (`-`)
 client.on('ready', message => {
@@ -20,15 +20,12 @@ client.on('ready', message => {
 });
 //This code gives someone the 'Amethysts' role when they join
 client.on('guildMemberAdd', (guildMember, member) => {
-  guildMember.addRole(guildMember.guild.roles.find(role => role.name === "Amethysts"));
-  guildMember.setNickname("Amethysts")
-  botlog.send(`Given ${guildMember.username} the Amethysts role and set their @ to Amethysts!`)
+  guildMember.addRole(guildMember.guild.roles.find(role => role.name === "Amethyst"));
 });
 //This code responds to chat messages
 client.on('message', message => {
-  const botlog = client.channels.get(`658789921489813515`)
   let args = message.content.substring(prefex.length).split(" ");
-  let amarole = message.guild.roles.find(role => role.name === "Amethysts");
+  let amarole = message.guild.roles.find(role => role.name === "Amethyst");
   //This is for the (-info) command
   if (message.content === "-info") {
     var authusernm = message.author.username
@@ -55,10 +52,6 @@ client.on('message', message => {
           "value": "You can say \"Hey, Amethyst!\" \"hey, amethyst!\" \"Hey Amethyst\" \"hey amethyst\" \"hey a\""
         },
         {
-          "name": "Wheres the perma link to the Famethyst's server?",
-          "value": "[Perm-Link](https://discord.io/Famethyst)(https://discord.io/Famethyst)"
-        },
-        {
           "name": "Want to add something?",
           "value": "Email me here => joshthekid234@gmail.com"
         }
@@ -67,42 +60,6 @@ client.on('message', message => {
     message.channel.send({
       embed
     });
-  }
-  //Version 3 info (-Version3 info)
-  if (message.content == `${prefex}Version3 info`) {
-    var authusernm = message.author.username
-    const embed = {
-      "title": "Version 3 Info",
-      "description": "Version 3 | look at the code on [Github](http://bit.ly/FamBotCode)",
-      "url": "http://bit.ly/FamBotCode",
-      "color": 65280,
-      "footer": {
-        "text": `Made by (Joshua Noakes#2986) | Version 3 info for ${authusernm} | Now running ${versnu}`
-      },
-      "image": {
-        "url": "https://vignette4.wikia.nocookie.net/steven-universe/images/8/8a/Amethyst_new.png/revision/latest/scale-to-width-down/1000?cb=20150430231339"
-      },
-      "fields": [{
-        "name": "What new features are there?",
-        "value": "There's now the \"Hey Amethyst Amethys\" command..."
-      }]
-    };
-    message.channel.send({
-      embed
-    });
-  }
-  // 5x @Everyone (Hey Amethyst Amethyst)
-  if (message.content == 'Hey Amethyst Amethyst') {
-    message.channel.send(`@everyone`)
-    for (i = 0; i < 5; i++) {
-      var authusernm = message.author.username
-      const embeda = new RichEmbed()
-        .setTitle("Hey, Amethysts")
-        .setImage("https://vignette4.wikia.nocookie.net/steven-universe/images/8/8a/Amethyst_new.png/revision/latest/scale-to-width-down/1000?cb=20150430231339")
-        .setColor("0xC49FD9")
-        .setFooter(`Hey, Amethyst from ${authusernm}`)
-      message.channel.send(embeda)
-    }
   }
   //* Below is code for all the (Hey, Amethyst!) commands
   if (message.content === 'Hey, Amethyst!') {
